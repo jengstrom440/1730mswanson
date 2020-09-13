67af3dafc8619590c015a38dfa3e35456b5e7e39
User <User@WINDEV2008EVAL>
2020-09-13 17:31:46 -0500

    completed 1c1

---
+++ mswanson1c1/frmFoodTruck.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace mswanson1c1
+{
+    partial class frmFoodTruck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHotDogs = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHotDogsSubtotal = new System.Windows.Forms.TextBox();
+            this.txtHamburglerSubTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHamgurglers = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtPretaxTotal = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hot dogs:";
+            // 
+            // txtHotDogs
+            // 
+            this.txtHotDogs.Location = new System.Drawing.Point(101, 44);
+            this.txtHotDogs.Name = "txtHotDogs";
+            this.txtHotDogs.Size = new System.Drawing.Size(39, 20);
+            this.txtHotDogs.TabIndex = 1;
+            this.txtHotDogs.Text = "0";
+            this.txtHotDogs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x $4.00";
+            // 
+            // txtHotDogsSubtotal
+            // 
+            this.txtHotDogsSubtotal.Location = new System.Drawing.Point(215, 44);
+            this.txtHotDogsSubtotal.Name = "txtHotDogsSubtotal";
+            this.txtHotDogsSubtotal.ReadOnly = true;
+            this.txtHotDogsSubtotal.Size = new System.Drawing.Size(76, 20);
+            this.txtHotDogsSubtotal.TabIndex = 3;
+            this.txtHotDogsSubtotal.TabStop = false;
+            this.txtHotDogsSubtotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtHamburglerSubTotal
+            // 
+            this.txtHamburglerSubTotal.Location = new System.Drawing.Point(215, 68);
+            this.txtHamburglerSubTotal.Name = "txtHamburglerSubTotal";
+            this.txtHamburglerSubTotal.ReadOnly = true;
+            this.txtHamburglerSubTotal.Size = new System.Drawing.Size(76, 20);
+            this.txtHamburglerSubTotal.TabIndex = 7;
+            this.txtHamburglerSubTotal.TabStop = false;
+            this.txtHamburglerSubTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x $5.00";
+            // 
+            // txtHamgurglers
+            // 
+            this.txtHamgurglers.Location = new System.Drawing.Point(101, 68);
+            this.txtHamgurglers.Name = "txtHamgurglers";
+            this.txtHamgurglers.Size = new System.Drawing.Size(39, 20);
+            this.txtHamgurglers.TabIndex = 5;
+            this.txtHamgurglers.Text = "0";
+            this.txtHamgurglers.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hamburglers:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(65, 321);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtPretaxTotal
+            // 
+            this.txtPretaxTotal.Location = new System.Drawing.Point(215, 94);
+            this.txtPretaxTotal.Name = "txtPretaxTotal";
+            this.txtPretaxTotal.ReadOnly = true;
+            this.txtPretaxTotal.Size = new System.Drawing.Size(76, 20);
+            this.txtPretaxTotal.TabIndex = 9;
+            this.txtPretaxTotal.TabStop = false;
+            this.txtPretaxTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(215, 120);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.ReadOnly = true;
+            this.txtTax.Size = new System.Drawing.Size(76, 20);
+            this.txtTax.TabIndex = 10;
+            this.txtTax.TabStop = false;
+            this.txtTax.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(215, 146);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(76, 20);
+            this.txtTotal.TabIndex = 11;
+            this.txtTotal.TabStop = false;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Pretax total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(157, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Tax:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(157, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Total:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(160, 321);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(253, 321);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmFoodTruck
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 370);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.txtPretaxTotal);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHamburglerSubTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHamgurglers);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHotDogsSubtotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHotDogs);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFoodTruck";
+            this.Text = "mswanson1c1:Food Truck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHotDogs;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHotDogsSubtotal;
+        private System.Windows.Forms.TextBox txtHamburglerSubTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHamgurglers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtPretaxTotal;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
